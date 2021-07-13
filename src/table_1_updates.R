f083a2dec9f774cb3778ebe141612d9c498e7958
Annie Paulukonis, M.S <elizabethpaulukonis@gmail.com>
2021-07-13 16:15:13 -0400

    add in multiple pieces of code to calculate summary, and updated table

--- src/table_1_updates.R
+++ src/table_1_updates.R
@@ -14,7 +14,7 @@ vm2015_file <- paste(amphibdir_data_in,"vm2014_data.csv", sep="")
 file.exists(vm2015_file)
 vm2015 <- read.table(vm2015_file, header = TRUE, sep = ",")
 
-vm_2014_atr<-filter(vm2015, Chemical== 'Atrazine' & Application == 'Soil') 
+#2014
 calc_2014<-vm2015 %>% 
   filter(Application == 'Soil') %>%
   group_by(Chemical, Species) %>% 
@@ -24,4 +24,22 @@ new_2014 <- calc_2014 %>%  unite(vals, min:avg, sep=", ")
 vm_2014_sum <- file.path(amphibdir,"data_in/vm2014_data_summarized.csv")
 write.csv(new_2014, vm_2014_sum)
 
+#2015
 
+
+
+
+##RVM 2014/2015 ----
+vm2015_file <- paste(amphibdir_data_in,"vm2014_data.csv", sep="")
+file.exists(vm2015_file)
+vm2015 <- read.table(vm2015_file, header = TRUE, sep = ",")
+
+vm_2014_atr<-filter(vm2015, Chemical== 'Atrazine' & Application == 'Soil') 
+calc_2014<-vm2015 %>% 
+  filter(Application == 'Soil') %>%
+  group_by(Chemical, Species) %>% 
+  summarise(min = round(min(BCF),3), max = round(max(BCF),3), avg = round(mean(BCF),3))
+
+new_2014 <- calc_2014 %>%  unite(vals, min:avg, sep=", ")
+vm_2014_sum <- file.path(amphibdir,"data_in/vm2014_data_summarized.csv")
+write.csv(new_2014, vm_2014_sum)
