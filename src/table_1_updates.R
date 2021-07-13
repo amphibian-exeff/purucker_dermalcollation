3dcc697796d9a87ce8e6a72160eea2c6c8954696
Annie Paulukonis, M.S <elizabethpaulukonis@gmail.com>
2021-07-13 19:11:03 -0400

    add in updated t1, and updated rmd

--- src/table_1_updates.R
+++ src/table_1_updates.R
@@ -8,6 +8,14 @@ amphibdir_data_out <- paste(amphibdir,'data_out/',sep='')
 amphibdir_graphics <- paste(amphibdir,'graphics/',sep='')
 amphibdir_src <- paste(amphibdir,'src/',sep='')
 
+#rvm 2014/2015 get read in via vm2014 just to double check that they are partitioned correctly
+#all other files get directed from the amphib dermal collated csv, BCF calculated
+all_derm_file <- paste(amphibdir_data_out,"updated_amphib_dermal_collated.csv", sep="")
+file.exists(all_derm_file)
+derm <- read.table(all_derm_file, header = TRUE, sep = ",")
+names(derm)
+
+derm$BCF<-derm$tissue_conc_ugg/derm$soil_conc_ugg
 
 ##RVM 2014/2015 ----
 vm2015_file <- paste(amphibdir_data_in,"vm2014_data.csv", sep="")
@@ -25,21 +33,23 @@ vm_2014_sum <- file.path(amphibdir,"data_in/vm2014_data_summarized.csv")
 write.csv(new_2014, vm_2014_sum)
 
 #2015
-
-
-
-
-##RVM 2014/2015 ----
-vm2015_file <- paste(amphibdir_data_in,"vm2014_data.csv", sep="")
-file.exists(vm2015_file)
-vm2015 <- read.table(vm2015_file, header = TRUE, sep = ",")
-
-vm_2014_atr<-filter(vm2015, Chemical== 'Atrazine' & Application == 'Soil') 
-calc_2014<-vm2015 %>% 
-  filter(Application == 'Soil') %>%
+calc_2015<-vm2015 %>% 
+  filter(Application == 'Overspray') %>%
   group_by(Chemical, Species) %>% 
   summarise(min = round(min(BCF),3), max = round(max(BCF),3), avg = round(mean(BCF),3))
 
-new_2014 <- calc_2014 %>%  unite(vals, min:avg, sep=", ")
-vm_2014_sum <- file.path(amphibdir,"data_in/vm2014_data_summarized.csv")
-write.csv(new_2014, vm_2014_sum)
+new_2015 <- calc_2015 %>%  unite(vals, min:avg, sep=", ")
+vm_2015_sum <- file.path(amphibdir,"data_in/vm2015_data_summarized.csv")
+write.csv(new_2015, vm_2015_sum)
+
+
+
+##RVM 2016 ----
+rvm2016<-filter(derm, source== 'rvm2016') 
+calc_rvm2016<-rvm2016 %>% 
+  group_by(chemical, species) %>% 
+  summarise(min = round(min(BCF),3), max = round(max(BCF),3), avg = round(mean(BCF),3))
+
+rvm_2016 <- calc_rvm2016 %>%  unite(vals, min:avg, sep=", ")
+rvm_2016_sum <- file.path(amphibdir,"data_in/vm2016_data_summarized.csv")
+write.csv(rvm_2016, rvm_2016_sum)
